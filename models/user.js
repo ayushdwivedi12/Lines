e63a8e25c0c45b2f8a58e2f97cd20a6d21e9c071
ayushdwivedi12 <pd8814312@gmail.com>
2025-07-02 00:55:16 +0530

    ✅ Fixed password hashing with crypto and salt

--- models/user.js
+++ models/user.js
@@ -35,33 +35,37 @@ const userSchema = new Schema(
   }
 );
 
+
 userSchema.pre("save", function (next) {
   const user = this;
-  if (!user.isModified("password")) return;
 
-  const salt = randomBytes(16).toString();
+  if (!user.isModified("password")) return next(); 
+
+  const salt = randomBytes(16).toString("hex"); 
   const hashedPassword = createHmac("sha256", salt)
     .update(user.password)
     .digest("hex");
 
-  this.salt = salt;
-  this.password = hashedPassword;
-  next();
+  user.salt = salt;
+  user.password = hashedPassword;
+
+  return next(); 
 });
+
 
 userSchema.static(
   "matchPasswordAndGenerateToken",
   async function (email, password) {
     const user = await this.findOne({ email });
     if (!user) throw new Error("User not Found");
-    const salt = user.salt;
-    const hashedPassword = user.password;
-    const userProvidedHash = createHmac("sha256", salt)
+
+    const userProvidedHash = createHmac("sha256", user.salt)
       .update(password)
       .digest("hex");
 
-    if (hashedPassword !== userProvidedHash)
+    if (user.password !== userProvidedHash) {
       throw new Error("Incorrect Password");
+    }
 
     const token = createTokenForUser(user);
     return token;
