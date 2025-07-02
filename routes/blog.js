3d14f45531c4909c8e6ef4cd88506fc8d038d995
ayushdwivedi12 <pd8814312@gmail.com>
2025-07-02 13:32:31 +0530

    🔧 Updated navbar, added search, and improved form validation

--- routes/blog.js
+++ routes/blog.js
@@ -7,6 +7,7 @@ const {
   handleGetAllBlogs,
 } = require("../controllers/blog");
 const { upload } = require("../middlewares/upload");
+const Blog = require("../models/blog");
 
 const router = express.Router();
 
@@ -14,10 +15,27 @@ router.get("/", handleGetAllBlogs);
 
 router.get("/add-blog", handleAddBlog);
 
-router.post("/", upload.single("CoverImage"), handleFileUpload);
-
-router.get("/:id", handleViewBlog);
+router.post("/", upload.single("coverImage"), handleFileUpload);
 
 router.post("/comment/:blogId", handleComments);
+
+router.get("/search", async (req, res) => {
+  const { title } = req.query;
+
+  try {
+    const blogs = await Blog.find({
+      title: { $regex: new RegExp(title, "i") },
+    });
+
+    res.render("home", {
+      blogs,
+      user: req.user,
+    });
+  } catch (err) {
+    res.status(500).send("Something went wrong");
+  }
+});
+
+router.get("/:id", handleViewBlog);
 
 module.exports = router;
